31b9724e8d7ca926ec768b404241cc8459ca6b36
egwilk28 <egwilk28@gmail.com>
2021-09-08 21:30:05 -0400

    initial commit

---
+++ spectralanalysis.R
@@ -0,0 +1,5 @@
+### This is an R script. Write code in the script.
+code(looks like this)
+"code" in line 2 is called a "function", while "looks like this" is called an "argument"
+# putting a pound sign in front of a line of code "comments it out" this is a good method of adding notes alongside your code so you don't foget what it does!
+#Please add code and changes all in the same script. Multiple people can't work simultaneously, but we use the repository to store versions with and without code, so don't worry about screwing anything up.
